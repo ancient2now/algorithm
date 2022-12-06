cfd2f2fa7a8c2d4fcfcebfd4fceb9739c9c355e6
李俊秋 <ancient2now@163.com>
2022-12-06 13:58:43 +0800

    add  Playgound.java

---
+++ leetcode/src/main/java/com/akikun/leetcode/_00000_Playgound.java
@@ -0,0 +1,19 @@
+package com.akikun.leetcode;
+
+/**
+ * 练习场地
+ *
+ * @author 李俊秋(龙泽)
+ */
+public class _00000_Playgound {
+
+    public static void main(String[] args) {
+        _00000_Playgound solution = new _00000_Playgound();
+
+
+        // 在这儿练习吧～
+
+
+    }
+
+}
